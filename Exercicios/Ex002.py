337072336ea8efc21cec62d87491d76652388140
Giliard <gil2614@live.com>
2022-02-05 21:21:10 -0300

    Exercício 02

---
+++ Exercicios/Ex002.py
@@ -0,0 +1,13 @@
+"""
+Exercício 2 ->Crie um Programa que eia o nome de uma pesooa de mostre uma mensagem de boas-vindas.
+
+"""
+
+# Respostas com várias saídas formatadas
+
+nome = input('Qual é o seu nome? ')
+print(f'Olá {nome}, Seja Bem vindo(a) ao Python!')
+print('============================')
+print('Olá {}, Seja bem vindo!'.format(nome))
+print('============================')
+print('Seja Bem vindo',nome)
