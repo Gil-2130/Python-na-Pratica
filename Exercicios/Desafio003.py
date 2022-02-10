83067631dcd3317d8d0021930bc31c25c4cd3c27
Giliard <gil2614@live.com>
2022-02-09 21:52:17 -0300

    Desafio003
    Desafio
    -> Crie um programa que leia dois numeros e mostre a soma entre eles.

---
+++ Exercicios/Desafio003.py
@@ -0,0 +1,13 @@
+"""
+Desafio
+-> Crie um programa que leia dois numeros e mostre a soma entre eles.
+"""
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro numero: '))
+s = n1 + n2
+print('A soma entre {} e {}, é {}.'.format(n1, n2, s))
+print('===================================')
+
+# ou
+
+print(f'A soma entre {n1} e {n2} é igual a {s}')
