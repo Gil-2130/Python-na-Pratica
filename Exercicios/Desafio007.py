f14d460520bba1a6968ab4f04743d3a57cbe8ba4
Giliard <gil2614@live.com>
2022-02-09 21:55:46 -0300

    Desafio007
    DEsenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média

---
+++ Exercicios/Desafio007.py
@@ -0,0 +1,7 @@
+"""
+DEsenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média
+"""
+nota1 = int(input('Qual foi a primeira nota? '))
+nota2 = int(input('Qual foi a segunda nota? '))
+media = (nota1 + nota2) / 2
+print('A nota média foi de {}'.format(media))
