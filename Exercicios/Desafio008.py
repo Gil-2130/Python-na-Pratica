646d3cb5f69df5a57ddc4ce8baf8829a59889101
Giliard <gil2614@live.com>
2022-02-11 17:07:14 -0300

    Desaio008
    Crie um programa que leia um valor em metros e o exiba convertido em centimetros
     e milimetros

--- Exercicios/Desafio008.py
+++ Exercicios/Desafio008.py
@@ -2,9 +2,12 @@
 Crie um programa que leia um valor em metros e o exiba convertido em centimetros
  e milimetros
 """
-met = int(input('Quantos metros deseja converter? '))
+met = float(input('Quantos metros deseja converter? '))
 cent = met * 100
 mili = met * 1000
-print('O valor convertido em centimetros é {}cm'.format(cent))
+print('O valor convertido em centimetros é {:.0f}cm'.format(cent))
 print('=' * 20)
-print('O valor convertido em milímetros é {}mm'.format(mili))
+print('O valor convertido em milímetros é {:.0f}mm'.format(mili))
+
+# Formatando melhor o print
+print('A medida {}, corresponde a {:.0f}cm e {:.0f}mm'.format(met, cent, mili))
