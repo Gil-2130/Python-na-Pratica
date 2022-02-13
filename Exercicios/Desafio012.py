d080c798af537186c441f4ad330d3fa9deef7e8f
Giliard <gil2614@live.com>
2022-02-13 20:34:07 -0300

    Desafio012
    Faça um algoritmo que leia o preço de um produto e mostre seu novo
    preço com 5% de desconto

--- Exercicios/Desafio012.py
+++ Exercicios/Desafio012.py
@@ -3,5 +3,6 @@ Faça um algoritmo que leia o preço de um produto e mostre seu novo
 preço com 5% de desconto
 """
 p1 = float(input('Digite o valor do Produto: '))
-p2 = p1-(p1*0.05)
+#p2 = p1-(p1*0.05)
+p2 = p1 - (p1 * 5 / 100)
 print('O novo valor com desconto é de {}$'.format(p2))
