fb9576a0492ed04420c21b6cdf2a99b050da7a67
Giliard <gil2614@live.com>
2022-02-13 20:14:43 -0300

    Desafio010
    Crie um programa que leia o quanto dinheiro uma pessoa tem na carteira
    e mostre quantos dolares ela pode comprar

--- Exercicios/Desafio010.py
+++ Exercicios/Desafio010.py
@@ -5,6 +5,6 @@ e mostre quantos dolares ela pode comprar
 
 # 1 Dólar (EUA) = 5,26 Real Brasileiro
 
-reais = float(input('Quantos reais você tem na carteira? '))
+reais = float(input('Quantos reais você tem na carteira? R$'))
 dolar = reais / 5.26
-print('Você com {} Reais poderia comprar {:.2f} Dólares'.format(reais, dolar))
+print('Você com {}R$ Reais poderia comprar {:.2f}R$'.format(reais, dolar))
