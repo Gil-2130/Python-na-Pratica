72079f8c70d0ba3c288cc95f2519225ee2dcd370
Giliard <gil2614@live.com>
2022-02-13 20:23:47 -0300

    Desafio011
    Faça um programa que leia a largura e a altura da parede em metros,
     em seguida calcule a sua área e a quantidade de tinta necessária para pintá-la,
     sabendo que cada litro de tinda pinta uma área de 2m²

--- Exercicios/Desafio011.py
+++ Exercicios/Desafio011.py
@@ -6,5 +6,5 @@ Faça um programa que leia a largura e a altura da parede em metros,
 alt_p = int(input('Medida da altura da parede? '))
 lar_p = int(input('Medida da largura da parede? '))
 area = alt_p * lar_p
-print('A area de pintura é de {} Metros Quadrados\n'.format(area),
-      'Serão necessários', area/2**2, 'Litros de tinta')
+print('A area de pintura é de {} m²\n'.format(area),
+      'Serão necessários', area/2, 'Litros de tinta')
