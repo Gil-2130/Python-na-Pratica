798e9de9dca4c408c213eb72c1a3f3ff78ca6496
Giliard <gil2614@live.com>
2022-02-15 20:34:22 -0300

    Desafio016
    Crie um programa que leia um número real qualquer e mostre na tela a sua porção inteira.

---
+++ Exercicios/Desafio016.py
@@ -0,0 +1,7 @@
+"""
+Crie um programa que leia um número real qualquer e mostre na tela a sua porção inteira
+"""
+
+from math import trunc
+num = float(input('Digite um número real: '))
+print('O numero digitado foi {} e sua porção inteira é {}'.format(num, trunc(num)))
