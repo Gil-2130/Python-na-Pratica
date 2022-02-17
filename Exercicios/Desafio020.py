487b6e6fec3b5400ce67b33c8fbab7b167ba0ccc
Giliard <gil2614@live.com>
2022-02-17 20:56:32 -0300

    Desafio020
    O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalho
    dos alunos.
    Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.

--- Exercicios/Desafio020.py
+++ Exercicios/Desafio020.py
@@ -3,3 +3,13 @@ O mesmo professor do desafio anterior quer sortear a ordem de apresentação de 
 dos alunos.
 Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
 """
+
+import random  # Podmos apenas importar o SHUFFLE -> from random import shuffle
+n1 = str(input('Primeiro nome? '))
+n2 = str(input('Segundo nome? '))
+n3 = str(input('Terceiro nome? '))
+n4 = str(input('Quarto nome? '))
+lista = [n1, n2, n3, n4]
+random.shuffle(lista)
+
+print(f'A ordem de apresentação é {lista}')
