afd3fabafbc4923d1485aca788a26d2c05c586ec
Giliard <gil2614@live.com>
2022-02-17 19:47:28 -0300

    Desafio017
    Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente
    de um triangulo retangulo, calcule e mostre o compriento da hipotenusa.

--- Exercicios/Desafio017.py
+++ Exercicios/Desafio017.py
@@ -3,9 +3,25 @@ Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente
 de um triangulo retangulo, calcule e mostre o compriento da hipotenusa.
 """
 
-# A minha Resposta
+# A minha Resposta (Importando Módulo)
 from math import sqrt, trunc
 cat_ops = float(input('Qual o valor do cateto oposto?: '))
 cat_adj = float(input('Qual é o valor do cateto adjacente? '))
 hipo_x = sqrt(pow(cat_ops, 2) + pow(cat_adj, 2))
 print('O valor da Hipotenusa é {:.2f}'.format(hipo_x))
+print('=' * 20)
+
+# Resposta do professor (Sem módulo)
+co = float(input('Comprimento do catetto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = (co**2 + ca**2) ** (1/2)
+print('A Hipotenusa vai medir {:.2f}'.format(hi))
+print('=' * 20)
+
+
+# Calculo de hipotenusa disponivel em python
+from math import hypot
+cat_ops = float(input('Comprimento do cateto oposto: '))
+cat_adj = float(input('Comprimento do cateto adjacente: '))
+hipo = hypot(cat_ops, cat_adj)
+print('A hipotenusa mede {:.2f}'.format(hipo))
