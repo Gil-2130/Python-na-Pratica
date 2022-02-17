8e9c9d1012f35d0efc04a6dbddab57e270d585e9
Giliard <gil2614@live.com>
2022-02-17 20:43:05 -0300

    Desafio019
    Um professor quer sortear um dos seus 04 alunos para apagar o quadro.
    Faça um programa que o ajude lendo o nome deles e escrevendo o nome do escolhido.

--- Exercicios/Desafio019.py
+++ Exercicios/Desafio019.py
@@ -2,3 +2,18 @@
 Um professor quer sortear um dos seus 04 alunos para apagar o quadro.
 Faça um programa que o ajude lendo o nome deles e escrevendo o nome do escolhido.
 """
+from random import choice
+
+names = 'maria', 'joao', 'pedro', 'gustavo'
+print('O nome escolhido foi {}.'.format(choice(names)))
+
+
+# Resposta do professor
+n1 = str(input('Primeiro Aluno? '))
+n2 = str(input('Segundo aluno? '))
+n3 = str(input('Terceiro aluno? '))
+n4 = str(input('Quarto aluno? '))
+lista = [n1, n2, n3, n4]
+
+escolhido = choice(lista)
+print('O aluno escolhido foi {}'.format(escolhido))
