73778cf128f160b3382e1682e4baf639b6c3b0ab
Giliard <gil2614@live.com>
2022-02-17 21:08:32 -0300

    Desafio021
    Faça um programa em python que abra e reproduza O AUDIO DE UM ARQUIVO MP3

--- Exercicios/Desafio021.py
+++ Exercicios/Desafio021.py
@@ -1,3 +1,8 @@
 """
 Faça um programa em python que abra e reproduza O AUDIO DE UM ARQUIVO MP3
 """
+import pygame
+pygame.init()
+pygame.mixer.music.load('musica que você deseja') # obs; precisa estar no mesmo diretório ok?
+pygame.mixer.music.play()
+pygame.event.wait()
