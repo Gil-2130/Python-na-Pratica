9597e4a8e8e50adaea8282edd0dd7fb537bf5d12
Giliard <gil2614@live.com>
2022-02-19 20:10:09 -0300

    Desafio026
    Crie um programa que leia uma frase pelo teclado e mostre:
    1. Quantas vezes aparece a letra A
    2. Em que posição ela aparece a primeira vez.
    3. Em que posição ela aparece a ultima vez

---
+++ Exercicios/Desafio026.py
@@ -0,0 +1,17 @@
+"""
+Crie um programa que leia uma frase pelo teclado e mostre:
+1. Quantas vezes aparece a letra A
+2. Em que posição ela aparece a primeira vez.
+3. Em que posição ela aparece a ultima vez
+"""
+
+frase = input('Digite qualquer frase: ')
+
+# Quantas vezes a letra aparece na frase
+print('A letra A aparece {} vezes na frase.'.format(frase.count('a')))
+
+# Posição que ela aparece a primeira vez
+print('indice {}'.format(frase.find('a')))
+
+# posição que ela aparece a ultima vez
+print('Indice {}'.format(frase.rfind('a')))
