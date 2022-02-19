d273df7f1d10890556c328e0184f9f7d5149a6a3
Giliard <gil2614@live.com>
2022-02-19 20:08:55 -0300

    Desafio024
    Crie um programa que leia o nome de uma cidade e diga se
    ela começa ou não com o nome 'SANTO'

---
+++ Exercicios/Desafio024.py
@@ -0,0 +1,9 @@
+"""
+Crie um programa que leia o nome de uma cidade e diga se
+ela começa ou não com o nome 'SANTO'
+"""
+
+cid_1 = 'São Paulo'
+cid_2 = 'santo antonio'
+print('santo' in cid_1,'\n',
+      'santo' in cid_2)
