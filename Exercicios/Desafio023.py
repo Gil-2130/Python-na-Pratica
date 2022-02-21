1c39e0c50190f7141342032d6aae3be059d01465
Giliard <gil2614@live.com>
2022-02-21 19:24:32 -0300

    Desafio023
    Faça um programa que leia um número de 0 a 9999 e mostre na tela
    cada um dos digitos separados.

--- Exercicios/Desafio023.py
+++ Exercicios/Desafio023.py
@@ -9,8 +9,12 @@ CENTENA: 8
 MILHAR: 1
 """
 
-n1 = input('Digite um noméro entre 0 e 9999: ')
-print('UNIDADE: {}\n'.format(n1[3]),
-      'DEZENA: {}\n'.format(n1[2]),
-      'CENTENA: {}\n'.format(n1[1]),
-      'MILHAR: {}'.format(n1[0]))
+n1 = int(input('Digite um numéro entre 0 e 9999: '))
+u = n1 // 1 % 10
+d = n1 // 10 % 10
+c = n1 // 100 % 10
+m = n1 // 1000 % 10
+print('UNIDADE: {}'.format(u))
+print('DEZENA: {}'.format(d))
+print('CENTENA: {}'.format(c))
+print('MILHAR: {}'.format(m))
