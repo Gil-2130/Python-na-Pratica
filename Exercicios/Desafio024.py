bca5b3607f0fb5679ea2ab0d408475aa3738ce18
Giliard <gil2614@live.com>
2022-02-21 19:38:25 -0300

    Desafio024
    Crie um programa que leia o nome de uma cidade e diga se
    ela começa ou não com o nome 'SANTO'

--- Exercicios/Desafio024.py
+++ Exercicios/Desafio024.py
@@ -3,7 +3,8 @@ Crie um programa que leia o nome de uma cidade e diga se
 ela começa ou não com o nome 'SANTO'
 """
 
-cid_1 = 'São Paulo'
-cid_2 = 'santo antonio'
-print('santo' in cid_1,'\n',
-      'santo' in cid_2)
+cid_3 = str(input('Qual a sua cidade? ')).strip()  # REmovendo os espaços em excesso se eles existirem
+
+# Verificando se a palavra digitada corresponde, porém devido ao case sensitive
+# pegamos o input, colocamos em maiusculo e fazemos a comparação
+print(cid_3[:5].upper() == 'SANTO')
