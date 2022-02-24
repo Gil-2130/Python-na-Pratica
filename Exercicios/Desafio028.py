45b0057636aa8f72bd9c92437f7c66c5f0111a18
Giliard <gil2614@live.com>
2022-02-24 20:23:12 -0300

    Add files via upload

---
+++ Exercicios/Desafio028.py
@@ -0,0 +1,7 @@
+"""
+Escreva um programa que faça o computador pensar em um numero inteiro entre 0 e 5
+e peça para o usuario tentar descobrir qual foi o numero escolhido
+pelo computador.
+
+O programa deverá escrever na tela se o usuario venceu ou perdeu
+"""
