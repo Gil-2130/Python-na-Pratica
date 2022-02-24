cb23ffd6e46eafe6cbd468de33d420472ef9427f
Giliard <gil2614@live.com>
2022-02-24 20:24:11 -0300

    Add files via upload

---
+++ Exercicios/Desafio031.py
@@ -0,0 +1,6 @@
+"""
+Desenvolva um programa que pergunte a distância de uma viagem em KM.
+
+Calcule o preço da passagem, cobrando R$0,50 por KM para viagens de até 200km
+e R$0,45 para viagens mais longas.
+"""
