38f1cf649764d471d7f978c692adc2e1daafa198
Giliard <gil2614@live.com>
2022-02-25 21:04:09 -0300

    Desafio031
    Desenvolva um programa que pergunte a distância de uma viagem em KM.
    
    Calcule o preço da passagem, cobrando R$0,50 por KM para viagens de até 200km
    e R$0,45 para viagens mais longas.

--- Exercicios/Desafio031.py
+++ Exercicios/Desafio031.py
@@ -4,3 +4,26 @@ Desenvolva um programa que pergunte a distância de uma viagem em KM.
 Calcule o preço da passagem, cobrando R$0,50 por KM para viagens de até 200km
 e R$0,45 para viagens mais longas.
 """
+
+# Forma 1
+viagem = float(input('Quantos quilômetros tem a viagem? '))
+if viagem <= 200:
+    print('O preço da passagem custará R${}'.format(viagem * 0.50))
+else:
+    print('O preço da passagem custará R${}'.format(viagem * 0.45))
+
+print('_==_' * 20)
+
+# forma 2
+viagem_1 = float(input('Quantos KM tem a viagem? '))
+print('Você está prestes a iniciar uma viagem de {}Km'.format(viagem_1))
+if viagem_1 <= 200:
+    preço = viagem_1 * 0.50
+else:
+    preço = viagem_1 * 0.45
+print('E o preço da sua passagem será {}'.format(preço))
+
+
+# ou
+#preco = viagem_2 * 0.50 if  viagem_2 <= 200 else viagem_2 *0.45
+# essa é uma forma válida, porém pode se tornar algo confuso para interpretação.
