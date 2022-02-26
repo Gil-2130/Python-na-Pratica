cd02da80522be36888e1582d174745b4b8fa2e9a
Giliard <gil2614@live.com>
2022-02-25 21:02:51 -0300

    Desafio030
    Crie um programa que leia um numero inteiro e mostre na tela se ele é par ou ímpar

--- Exercicios/Desafio030.py
+++ Exercicios/Desafio030.py
@@ -1,3 +1,9 @@
 """
 Crie um programa que leia um numero inteiro e mostre na tela se ele é par ou ímpar
 """
+
+num = int(input('Digite um numero: '))
+if num % 2 == 0:
+    print('O número {} é par!'.format(num))
+else:
+    print('O número {} é ímpar!'.format(num))
