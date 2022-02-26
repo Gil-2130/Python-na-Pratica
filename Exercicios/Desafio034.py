03883f859c5f721ce710b52fb7978db50eb113c7
Giliard <gil2614@live.com>
2022-02-25 21:06:26 -0300

    Desafio034
    Escreva um programa que pergunte o salário de um funcionário e calcule
    o valor do seu aumento.
    
    Para salários superiores a R$1.250,00, calcule um aumento de 10%
    
    Para os inferiores ou iguais, aumento é de 15%

--- Exercicios/Desafio034.py
+++ Exercicios/Desafio034.py
@@ -6,3 +6,11 @@ Para salários superiores a R$1.250,00, calcule um aumento de 10%
 
 Para os inferiores ou iguais, aumento é de 15%
 """
+
+salario = float(input('Qual é o seu salário? '))
+if salario <= 1250:
+    print('OK, seu atual salario de R${} sofrerá reajuste de 15%\n'.format(salario),
+          'Seu novo salário será de R${}'.format(salario + (salario * 0.15)))
+else:
+    print('OK, seu atual salario de R${} sofrerá reajuste de 10%\n'.format(salario),
+          'Seu novo salário será de R${}'.format(salario + (salario * 0.10)))
