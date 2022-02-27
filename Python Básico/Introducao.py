9cdb05b0c0724119749fef58ff1f8c775ffe0eda
Giliard <gil2614@live.com>
2022-02-27 20:25:02 -0300

    Aula01
    Introdução a variaveis, strings e numeros

---
+++ Python Básico/Introducao.py
@@ -0,0 +1,17 @@
+# Usando o print, variaveis, strings e numeros
+
+# Variavel armazenando string
+nome = 'giliard'   
+
+#Variavel armazenando numeros
+idade = 34
+peso = 70
+
+# Imprimindo na tela
+print(nome, idade, peso)
+
+# Usando o input
+nome = input('Qual é o seu nome? ')
+idade = float(input('Qual é a sua idade? '))
+peso = float(input('Agora sem mentir...qual é o seu peso? '))
+print(f'Olá {nome}\nsua idade é {idade} anos e você tem {peso} kilos!')
