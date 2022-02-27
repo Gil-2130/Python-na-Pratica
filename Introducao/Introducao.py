2608e046a3b8668c9557d43596faf5e870dc7a31
Giliard <gil2614@live.com>
2022-02-27 20:21:26 -0300

    Aula01
    Primeiros passos!

---
+++ Introducao/Introducao.py
@@ -0,0 +1,11 @@
+# Usando o print, variaveis, strings e numeros
+nome = 'giliard'
+idade = 34
+peso = 70
+print(nome, idade, peso)
+
+# Usando o input
+nome = input('Qual é o seu nome? ')
+idade = float(input('Qual é a sua idade? '))
+peso = float(input('Agora sem mentir...qual é o seu peso? '))
+print(f'Olá {nome}\nsua idade é {idade} anos e você tem {peso} kilos!')
