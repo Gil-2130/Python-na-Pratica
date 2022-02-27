20f3accf4e03ef3b462361da0f4e7e2e363d9049
Giliard <gil2614@live.com>
2022-02-27 20:18:57 -0300

    Add files via upload

---
+++ Exercicios/Desafio036.py
@@ -0,0 +1,9 @@
+"""
+Desenvolva um programa para aprovar um emprestimo bancário para a compra
+de uma casa.
+O programa vai perguntaro valor da casa, o salário do comprador
+e em quantos anos ele vai pagar.
+
+calcule o valor da prestação mensal sabendo que ele não pode exceder
+30% do salário ou então o empréstimo será negado.
+"""
