60feb771677f3a4372218ecfa1c4257ed783bd81
Giliard <gil2614@live.com>
2022-02-28 15:30:04 -0300

    Add files via upload

---
+++ Exercicios/Desafio044.py
@@ -0,0 +1,9 @@
+"""
+Elabore um programa que calcule o valor a ser oago por um produto,
+considerando o seu preço normal e condição de pagamento:
+
+- à vista dinheiro/cheque: 10% de desconto
+- à vista no cartão: 5% de desconto
+- Em até duas vezes no cartão: Prço normal.
+- 3x ou mais no cartão: 20% de jutos
+"""
