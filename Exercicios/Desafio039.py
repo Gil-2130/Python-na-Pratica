5579e090dd8f3d312e8389e1c6d97ef7c2002774
Giliard <gil2614@live.com>
2022-02-28 15:27:58 -0300

    Add files via upload

---
+++ Exercicios/Desafio039.py
@@ -0,0 +1,8 @@
+""" Faça um programa que leia o ano de nascimento de uma
+pessoa e informe de acordo com a sua idade;
+ - Se ele ainda vai se alistar ao serviço militar.
+ - Se é hora de se alistar
+ - Se já passou do tempo de se alistamento.
+
+ O programa ainda deverá mostrar o tempo que falta ou que passou do prazo
+"""
