4f9c991bdad1c12a46a74d20f0f381c33bb05689
Giliard <gil2614@live.com>
2022-02-28 15:27:23 -0300

    Add files via upload

---
+++ Exercicios/Desafio037.py
@@ -0,0 +1,7 @@
+"""
+Desenvolva um programa que leia um numero inteiro qualquer
+e peça para o usuário escolher qual será a base de conversão
+1. para Binário
+2. Para Octal
+3. Para Hexadecimal
+"""
