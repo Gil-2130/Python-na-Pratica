52e986b3eb7fa09bab7eeeaabf21c0ca7f491822
Giliard <gil2614@live.com>
2022-02-28 15:29:24 -0300

    Add files via upload

---
+++ Exercicios/Desafio042.py
@@ -0,0 +1,8 @@
+"""
+REfaça o desafio035 dos triangulos, acrescentando o recurso de mostrar
+que tipo de triangulo será formado
+
+ - EQUILATERO: todos os lados iguais
+ - ISÓSCELES: dois lados iguais
+ - ESCALENO: Todos os lados diferentes
+"""
