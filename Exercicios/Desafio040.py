061aa6e9f8b46de60073af63129bec6d2f2aae1e
Giliard <gil2614@live.com>
2022-02-28 15:28:45 -0300

    Desafio040
    Crie um programa que leia 02 notas de um aluno e calcule sua média,
    mostrando uma mensagem no final, de acordo coma média atingida:
    
     - Média abaixo de 5: REPROVADO
     - Média entre 5 e 6.9: RECUPERAÇÃO
     - Média 7 ou superior: APROVADO

---
+++ Exercicios/Desafio040.py
@@ -0,0 +1,17 @@
+"""
+Crie um programa que leia 02 notas de um aluno e calcule sua média,
+mostrando uma mensagem no final, de acordo coma média atingida:
+
+ - Média abaixo de 5: REPROVADO
+ - Média entre 5 e 6.9: RECUPERAÇÃO
+ - Média 7 ou superior: APROVADO
+"""
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digie a segunda nota: '))
+media = (n1 + n2) / 2
+if media < 5:
+    print('REPROVADO! Estude mais')
+elif media >= 5 and media <= 6.9:
+    print('RECUPERAÇÃO! Cuidado, você deve melhorar.')
+elif media >= 7:
+    print('PARABÈNS! Você foi aprovado!')
