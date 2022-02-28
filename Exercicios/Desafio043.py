d6f8631bd7a9e4b7e409f9060fdaa0b668876efc
Giliard <gil2614@live.com>
2022-02-28 15:29:44 -0300

    Add files via upload

---
+++ Exercicios/Desafio043.py
@@ -0,0 +1,10 @@
+"""
+Desenvolva uma lógica que leia o peso e altura de uma pessoa,
+calcule seu IMC e mostre seu staus de acordo coma atabela abixo:
+
+ - Abaixo de 18,5 : Abaixo do peso
+ - Enre 18,5 e 25 : Peso ideal
+ - entre 25 a 30 : Sobrepeso
+ - entre 30 e 40 : Obesidade
+ - Acima de 40 : Obesidade mórbida
+"""
