74f09a5ef6315fe7d0ad89de62332107f729fa0d
Giliard <gil2614@live.com>
2022-03-02 16:41:37 -0300

    Desafio036
    Desenvolva um programa para aprovar um emprestimo bancário para a compra
    de uma casa.
    O programa vai perguntar o valor da casa, o salário do comprador
    e em quantos anos ele vai pagar.
    
    calcule o valor da prestação mensal sabendo que ele não pode exceder
    30% do salário ou então o empréstimo será negado.

--- Exercicios/Desafio036.py
+++ Exercicios/Desafio036.py
@@ -1,9 +1,29 @@
 """
 Desenvolva um programa para aprovar um emprestimo bancário para a compra
 de uma casa.
-O programa vai perguntaro valor da casa, o salário do comprador
+O programa vai perguntar o valor da casa, o salário do comprador
 e em quantos anos ele vai pagar.
 
 calcule o valor da prestação mensal sabendo que ele não pode exceder
 30% do salário ou então o empréstimo será negado.
 """
+
+# Valor da casa
+casa = float(input('Qual o valor da casa? '))
+
+# Salário por Mês
+salario = float(input('Qual é o seu salário? '))
+
+# Quantidade de anos para pagamento
+ano = float(input('Em quantos anos pretende pagar? ')) * 12
+
+# Valor da prestação
+prestação = casa / ano
+
+# Calculando os 30% da prestação para pagamento
+if prestação <= prestação * 0.30:
+    print('Parabéns! Seu crédito foi aprovado!\n'
+          'Sua prestação será de R${:.2f} mensais'.format(prestação))
+else:
+    print('Que pena! Não podemos aprovar sua solicitação!')
+print('Obrigado pelo contato!')
