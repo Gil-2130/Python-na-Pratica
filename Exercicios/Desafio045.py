f41148a3745c683d52d6020b4fa60f98dfdec1e4
Giliard <gil2614@live.com>
2022-03-03 16:54:11 -0300

    Desafio045
    Crie um programa que faça o computador jogar jokenpô com você.

--- Exercicios/Desafio045.py
+++ Exercicios/Desafio045.py
@@ -1,9 +1,7 @@
 """
 Crie um programa que faça o computador jogar jokenpô com você.
 """
-
-# minha solução
-print('{:=^40}'.format(' JOKENPÔ '))
+print('{:=^40}'.format(' JOKENPO '))
 
 print('=' * 40)
 print('Pedra, Papel ou tesoura?\n'
@@ -59,3 +57,87 @@ elif opcao == 3 and pc == 1:
 else:
     opcao = 0
     print('Opção inválida!!!')
+print('-=-' * 15)
+
+
+
+
+# Resolução do professor
+
+# Importando módulo para escolher randomicamente um número
+from random import randint
+
+# Importando módulo para sleep
+from time import sleep
+
+# colocando as opões em uma tupla
+itens = ('PEDRA', 'PAPEL', 'TESOURA')
+
+# opçoes do PC
+pc = randint(1, 3)
+
+# imprimindo as opções para o usuario
+print('''Suas opções:
+[ 1 ] PEDRA
+[ 2 ] PAPEL
+[ 3 ] TESOURA''')
+
+# Solicitando ao usuário que digite uma opção
+player = int(input('Sua opção: '))
+
+# Imprimindo JOKENPO
+print('JO...', end='')
+sleep(0.8)
+print('KEN...', end='')
+sleep(0.8)
+print('PO!!')
+
+
+# Imprimindo um separador para organização do código
+print('-=-' * 15)
+
+# Imprimindo a opção do PC
+print('PC escolheu {}'.format(itens[pc]))
+
+# Imprimindo a opção do player
+print('PLAYER escolheu {}'.format(itens[player]))
+
+# Adicionando novo separador
+print('-=-' * 15)
+
+# Condição aninhada ao PC escolher 1 (PEDRA)
+if pc == 1:
+    if player == 1:
+        print('EMPATE!!!')
+    elif player == 2:
+        print('PLAYER VENCEU!!!')
+    elif player == 3:
+        print('PC VENCEU!!!')
+    else:
+        player = 0
+        print('Opção inválida!!!')
+
+# Condição aninhada ao PC escolher 2 (PAPEL)
+elif pc == 2:
+    if player == 1:
+        print('PC VENCEU!!!')
+    elif player == 2:
+        print('EMPATE!!!')
+    elif player == 3:
+        print('PLAYER VENCEU!!!')
+    else:
+        player = 0
+        print('Opção invãlida!!!')
+
+# Condição aninhada ao PC escolher 3 (TESOURA)
+elif pc == 3:
+    if player == 1:
+        print('PLAYER VENCEU!!!')
+    elif player == 2:
+        print('PC VENCEU!!!')
+    elif player == 3:
+        print('EMPATE!!!')
+    else:
+        player = 0
+        print('Opção Inválida!!!')
+
