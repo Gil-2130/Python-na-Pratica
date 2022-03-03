2c3ef837a2258ff13ad2b763c3ca2e58f85fd3df
Giliard <gil2614@live.com>
2022-03-03 16:10:14 -0300

    Desafio013
    Faça um algoritmo que leia o salario de um funcionário
    e mostre seu novo salario com 15% de aumento.

--- Exercicios/Desafio013.py
+++ Exercicios/Desafio013.py
@@ -3,8 +3,10 @@ Faça um algoritmo que leia o salario de um funcionário
 e mostre seu novo salario com 15% de aumento
 """
 
+# Obtendo o salário
 sal = float(input('Qual o seu salario? '))
 #n_sal = sal + (sal * 0.15)
+# Calculando o novo salário
 n_sal = sal + (sal * 15 / 100)
 print('Seu antigo salário era de {}$\n'.format(sal),
       'Agora seu novo salário com ajuste de 15% é de {}$'.format(n_sal))
