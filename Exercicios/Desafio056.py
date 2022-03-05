96ef4730d53865ce5e0f534f9a0c02dffc6ffc31
Giliard <gil2614@live.com>
2022-03-05 20:15:44 -0300

    Add files via upload

---
+++ Exercicios/Desafio056.py
@@ -0,0 +1,7 @@
+"""
+desenvolva um programa que leia o nome, idade e sexo de 04 pessoas.
+No final do programa mostre:
+1. A média de idade do grupo.
+2. Qual é o nome do homem mais velho
+3. Quantas mulheres tem menos de 20 anos
+"""
