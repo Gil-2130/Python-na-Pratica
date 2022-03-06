03e8c36ce8f3f2eac5114d5ec24999ecdd964f88
Giliard <gil2614@live.com>
2022-03-06 15:48:17 -0300

    Desafio049
    Refaça o desafio 009, mostrando a tabuada de um número que o usuário escolher
    porém utilizando o laço for.

--- Exercicios/Desafio049.py
+++ Exercicios/Desafio049.py
@@ -2,3 +2,9 @@
 Refaça o desafio 009, mostrando a tabuada de um número que o usuário escolher
 porém utilizando o laço for.
 """
+
+num = int(input('Digite um número e sua tabuada(x) será calculada: '))
+for i in range(0, 11):
+    d = i
+    i = i * num
+    print('{} x {} = {}'.format(num, d, i))
