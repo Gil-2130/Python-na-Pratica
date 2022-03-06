613b29ba44490bd4ba82e8718f1d9598fe10909e
Giliard <gil2614@live.com>
2022-03-06 16:58:55 -0300

    Add files via upload

---
+++ Exercicios/Desafio050.py
@@ -0,0 +1,26 @@
+"""
+desenvolva um programa que leia 6 numeros inteiros e mostre a soma apenas daqueles que forem pares.
+Se o valor digitado for impar, desconsidere-o
+"""
+
+# Variavel que armazenará os valores
+soma = 0
+
+# laço de repetição do input
+for i in range(0, 6):
+
+    # Input dos dados
+    n = int(input('Digite um número: '))
+
+    # Condicional para guardar apenas números pares
+    if n % 2 == 0:
+
+        # adicionado o resultado do input à variavel
+        soma = soma + n
+        #print('')      não imprima nada, apenas faça a adição dos números pares
+
+    else:
+        print('NÚMEROS ÍMPARES NÃO PERMITIDOS!!')
+
+# Imprimindo os resultados
+print(soma)
