8a4d24d0a52e2ed7c3a16f23c9dbfef4e13a2dad
Giliard <gil2614@live.com>
2022-03-06 14:10:45 -0300

    Desafio047
    Crie um programa que mostre na tela todos os numeros pares que estão no intervalo
    entre 1 e 50.

--- Exercicios/Desafio047.py
+++ Exercicios/Desafio047.py
@@ -2,3 +2,10 @@
 Crie um programa que mostre na tela todos os numeros pares que estão no intervalo
 entre 1 e 50.
 """
+
+# laço dentro de um range de valores
+for lista in range(1, 51):
+
+    # Condicional para imprimir apenas os numeros pares
+    if lista % 2 == 0:
+        print(lista)
