74bdbedce95ddb804d9b8cb3043f44425d01288f
Giliard <gil2614@live.com>
2022-03-07 17:11:13 -0300

    Desafio048
    Faça um programa que calcule a soma entre todos os numeros ímpares
    que são multiplos de três e que se encontram no intervalo entre 1 até 500

--- Exercicios/Desafio048.py
+++ Exercicios/Desafio048.py
@@ -2,8 +2,11 @@
 Faça um programa que calcule a soma entre todos os numeros ímpares
 que são multiplos de três e que se encontram no intervalo entre 1 até 500
 """
-# Variavel (acumulador) para guardar o resultado do laço
+# Variavel acumuladora para guardar o resultado do laço
 soma = 0
+
+# Variável contadora para saber quantos números foram encontrados
+cont = 0
 
 # laço atendendo os requisitos do problemas
 for i in range(1, 501):
@@ -11,10 +14,14 @@ for i in range(1, 501):
     # Condicional para números ímpares e multiplos de 03
     if i % 3 == 0 and i % 2 == 1:
 
-        # Com a condição atendida, iremos guardar o resultado na variavel
+        # Com a condição atendida, iremos guardar o resultado na variavel (acumulador)
         soma = soma + i
         
+        # verificando quantos números foram obtidos
+        cont= cont + 1
+
         # Este print não é necessário, então podemos remover.
         print(i)
 # Por fim imprimimos os resultados
-print(soma)
+print('Foram encontrados {} números nessa sequência'.format(cont))
+print('A soma total foi de {}'.format(soma))
