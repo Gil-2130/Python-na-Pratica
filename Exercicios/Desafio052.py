737b93f6be6b4b1baface75b4da0cb947887e9a0
Giliard <gil2614@live.com>
2022-03-08 14:21:12 -0300

    Desafio052
    Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.

--- Exercicios/Desafio052.py
+++ Exercicios/Desafio052.py
@@ -1,3 +1,27 @@
 """
 Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
 """
+# Solicitando um número
+n = int(input('Digite um número: '))
+
+# variavel para armazenar o total
+total = 0
+
+# Laço de repetição
+for i in range(1, n + 1):
+    
+    # Condicional da divisão do input pelo iterador no range
+    if n % i == 0:
+        print('\033[33m', end='')
+        
+        # adicionado á variavel
+        total += 1
+    else:
+        print('\033[31m', end='')
+    print('{} '.format(i), end='')
+
+print('\n\033[m0 Número {} foi divisivel {} vezes.'.format(n, total))
+if total == 2:
+    print('Este é um número primo!')
+else:
+    print('Este não é um numero primo!')
