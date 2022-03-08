41c47571a90e54566e8f463df0be3e09cc33c451
Giliard <gil2614@live.com>
2022-03-08 17:32:50 -0300

    Desafio055
    Faça um programa que leia o peso de 5 pessoas. No final, mostre qual foi o maior e o menor peso lidos.

--- Exercicios/Desafio055.py
+++ Exercicios/Desafio055.py
@@ -1,3 +1,25 @@
 """
 Faça um programa que leia o peso de 5 pessoas. No final, mostre qual foi o maior e o menor peso lidos.
 """
+
+# Inicializando variaveis que irão armazenar peso minimo e máximo
+p_max = 0
+p_min = 0
+# Criando laço para solicitar o peso das pessoas (5)
+for i in range(1, 6):
+    peso = float(input('Peso da {}ª pessoa: '.format(i)))
+
+    # Condicionais
+    if i == 1:
+        p_max = peso
+        p_min = peso
+
+    else:
+        if peso > p_max:
+            p_max = peso
+        elif peso < p_min:
+            p_min = peso
+
+
+print('O maior peso lido foi {}Kg'.format(p_max))
+print('O menor peso lido foi {}Kg'.format(p_min))
