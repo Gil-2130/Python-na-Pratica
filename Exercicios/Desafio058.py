703dac0cc5ff7c7ddc4776dad27c6c80a40add42
Giliard <gil2614@live.com>
2022-03-09 13:37:55 -0300

    Add files via upload

---
+++ Exercicios/Desafio058.py
@@ -0,0 +1,5 @@
+"""
+Melhore o jogo do DESAFIO28 onde o computador vai pensar em um numero entre 0 e 10
+
+Porém, o player tentara acertar, mostrando no final, quantos palpites foram necessários para acertar
+"""
