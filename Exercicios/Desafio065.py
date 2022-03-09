30b249c85a6a5b710e487c071465559ddf5bbd5c
Giliard <gil2614@live.com>
2022-03-09 13:40:52 -0300

    Add files via upload

---
+++ Exercicios/Desafio065.py
@@ -0,0 +1,8 @@
+"""
+Crie um programa que leia vários números inteiros pelo teclado.
+
+No Final da execução, mostre a média entre todos os valores e qual foi o MAIOR e MENOR
+valores lidos.
+
+O programa deve Perguntar ao usuario se ele quer ou não continuar a digitar valores.
+"""
