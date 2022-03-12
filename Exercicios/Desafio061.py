de7fcbe8964022674ba3eb0cf76c1c5bfb0994a3
Giliard <gil2614@live.com>
2022-03-12 18:09:46 -0300

    Desafio061
    Refaça o desafio 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10
    primeiros termos da progressão usando a estrutura WHILE

--- Exercicios/Desafio061.py
+++ Exercicios/Desafio061.py
@@ -2,3 +2,18 @@
 Refaça o desafio 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10
 primeiros termos da progressão usando a estrutura WHILE
 """
+
+print('Gerando PA')
+print('=-=' * 10)
+
+prim = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = prim
+cont = 1
+
+while cont <= 10:
+    print('{} ⇒ '.format(termo), end='')
+    termo = termo + razao
+    cont = cont + 1
+
+print('FIM')
