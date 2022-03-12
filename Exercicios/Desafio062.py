0fa15824eb6fbed570788bf531e7cb1675f7845b
Giliard <gil2614@live.com>
2022-03-12 19:35:24 -0300

    Desafio062
    Melhore o desafio 061, perguntando para o usuário se ele quer mostrar mais alguns termos.
    
    O programa encerra quando ele disser que quer mostrar 0 termos.

--- Exercicios/Desafio062.py
+++ Exercicios/Desafio062.py
@@ -4,3 +4,48 @@ Melhore o desafio 061, perguntando para o usuário se ele quer mostrar mais algu
 O programa encerra quando ele disser que quer mostrar 0 termos.
 
 """
+
+# Imprimindo msg de Geração de PA
+print('Gerando PA')
+print('=-=' * 10)
+
+# Obtendo o primeiro termo
+prim = int(input('Primeiro Termo: '))
+
+# Razão da PA
+razao = int(input('Razão da PA: '))
+
+# guardando o resultado do input do primeiro termo
+termo = prim
+
+# Criando contador
+cont = 1
+
+# Imprimindo total de PA's
+total = 0
+
+# Armzenando novos termos
+mais = 10
+
+# Laço
+while mais != 0:
+
+    # armazendo a variavel responsável por solicitar mais PA's
+    total = total + mais
+
+    # Laço
+    while cont <= total:
+        print('{} ⇒ '.format(termo), end='')
+        termo = termo + razao
+        cont = cont + 1
+    # Imprimindo uma pausa (Fictícia)
+    print('PAUSA')
+
+    # Variavel para solicitar mais termos caso o usuário deseje
+    mais = int(input('Quantos termos a mais, você deseja mostrar? '))
+
+print('Progressão finalizada com {} termos!'.format(total))
+print('=-=' * 10)
+
+# Imprimindo o fim do programa
+print('FIM!!')
