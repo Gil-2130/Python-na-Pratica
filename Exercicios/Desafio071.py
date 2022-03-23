ff585e954881025af15b753c1f8a9cb90c278ccc
Giliard P. Santos <gil2614@live.com>
2022-03-22 21:04:44 -0300

    Update Desafio071.py

--- Exercicios/Desafio071.py
+++ Exercicios/Desafio071.py
@@ -3,3 +3,39 @@ No inicio, pregunte ao usuário qual será o valor a ser sacado(inteiro)
 Seu programa deve informar quantas cédulas de cada valor serão entregues.
 
 Obs; Considere que o caixa possui cédulas de 50, 20, 10 e 1
+
+print('='*30)
+print('{:=^30}'.format(' BANCO PYTHON '))
+print('='*30)
+
+valor = int(input('Qual a Quantia deseja sacar hoje? R$'))
+
+total = valor
+cedula = 50
+total_ced = 0
+
+while True:
+    if total >= cedula:
+        total = total - cedula
+        total_ced = total_ced + 1
+
+    else:
+        if total_ced > 0:
+            print(f'Total de {total_ced} cédulas de R${cedula}')
+
+        if cedula == 50:
+            cedula = 20
+
+        elif cedula == 20:
+            cedula = 10
+
+        elif cedula == 10:
+            cedula = 1
+
+        total_ced = 0
+
+        if total == 0:
+            break
+
+print('=' * 30)
+print('Obrigado por ser cliente do BANCO PYTHON!!')
