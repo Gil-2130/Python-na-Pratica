e5afbfec204b73aae0bedae32eae1fa8840efa51
Giliard P. Santos <gil2614@live.com>
2022-03-24 11:01:03 -0300

    Add files via upload

---
+++ Exercicios/Desafio073.py
@@ -0,0 +1,9 @@
+"""
+Crie uma tupla preenchida com os 20 primeiros colocados da tabela
+do campeonato brasileiro de Futebol, na ordem de colocação.
+DEpois mostre:
+A). Apenas os 5 primeiros colocados.
+B). Os últimos 04 colocados da tabela.
+C). Uma lista com os times em ordem alfabética.
+D). Em que posição está o time da chapecoense.
+"""
