23c17102a49f75ef85314fdea6635537ab07ecd6
Giliard P. Santos <gil2614@live.com>
2022-03-24 11:02:28 -0300

    Add files via upload

---
+++ Exercicios/Desafio075.py
@@ -0,0 +1,9 @@
+"""
+Desenvolva um programa que leia 04 valores pelo teclado
+e guarde-os em uma tupla.
+
+No final mostre:
+A). Quantas vezes apareceu o valor 9
+B). E que posição foi digitado o primeiro valor 3
+C). Quais foram os números pares.
+"""
