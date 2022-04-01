307f5f49cec09ca9adc43ff8c16eb56810623973
Giliard P. Santos <gil2614@live.com>
2022-04-01 16:11:27 -0300

    Desaio076
    Crie um programa que tenha uma tupla única com nomes de produtos
    e seus respectivos preços, na sequência.
    
    No final, mostre uma listagem de preços, organinzando
    os dados em forma tabular.

--- Exercicios/Desafio076.py
+++ Exercicios/Desafio076.py
@@ -5,3 +5,8 @@ e seus respectivos preços, na sequência.
 No final, mostre uma listagem de preços, organinzando
 os dados em forma tabular.
 """
+
+cesta = ('leite: 2.50R$', 'café: 5.80R$', 'açucar: 3.50R$', 'arrox: 10.0R$')
+
+for i in cesta:
+    print(i)
