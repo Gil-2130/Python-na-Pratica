d29f33342706f359d1cd1a36fd4dca0342e8ec73
Giliard P. Santos <gil2614@live.com>
2022-04-03 14:13:04 -0300

    Desafio077
    Crie um programa que tenha uma tupla com várias palavras(Não use acentos).
    Depois disso, você deve mostrar, para cada palavra
    quais são as suas vogais.

--- Exercicios/Desafio077.py
+++ Exercicios/Desafio077.py
@@ -1,5 +1,24 @@
 """
-Crie um programa que tenha um atupla com várias palavras(Não use acentos).
+Crie um programa que tenha uma tupla com várias palavras(Não use acentos).
 Depois disso, você deve mostrar, para cada palavra
 quais são as suas vogais.
 """
+# Criando a tupla de palavras
+palavras = ('maria', 'jose', 'pedro', 'macarrao')
+
+
+# Realizando um laço nas palavras da tupla
+for palavra in palavras:
+    # Imprimindo as palavras
+    print(f'\nNa palavra {palavra.upper()} temos as vogais: ')
+    
+    # laço de letras nas palavras
+    for letra in palavra:
+    
+        # Condicional para buscar apenas as letras
+        if letra.lower() in 'aeiou':
+            print(letra.upper())
+    
+        # Este else é opcional, pois queremos apenas as vogais.
+        else:
+            print('Esta não é uma vogal...')
