90a538a0ba5daa290ad8fa0fcaea4662ee874f02
Giliard P. Santos <gil2614@live.com>
2022-04-06 20:00:20 -0300

    Add files via upload

---
+++ Exercicios/Desafio079.py
@@ -0,0 +1,8 @@
+"""
+Crie um programa onde o usuário possa digitar vários valoes numéricos e
+cadastre-os em uma lista.
+
+Caso o número já exista lá dentro, ele não será adicionado.
+
+No final, serã exibidos todos os valores únicos digitados, em ordem crescente.
+"""
