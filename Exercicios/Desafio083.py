8ba54b4a01fa45e4d51d1b4566ecf4c40952504e
Giliard P. Santos <gil2614@live.com>
2022-04-06 20:12:19 -0300

    Add files via upload

---
+++ Exercicios/Desafio083.py
@@ -0,0 +1,5 @@
+"""Crie um programa on de o usuário digite uma expressão qualquer
+e que use parenteses.
+Seu programa deverá analisar se a expressão passada
+está com os parenteses abertos e fechados naordem correta
+"""
