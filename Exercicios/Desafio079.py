0e5ee98fe67283a4ed5f47a1f1d46fa4db8afbc7
Giliard P. Santos <gil2614@live.com>
2022-04-09 15:43:03 -0300

    Desafio079
    Crie um programa onde o usuário possa digitar vários valoes numéricos e
    cadastre-os em uma lista.
    
    Caso o número já exista lá dentro, ele não será adicionado.
    
    No final, serã exibidos todos os valores únicos digitados, em ordem crescente.

--- Exercicios/Desafio079.py
+++ Exercicios/Desafio079.py
@@ -11,30 +11,38 @@ numeros = list()
 
 # Criando um laço infinito
 while True:
+    
     # Criando uma variavel que receberá os inputs do usuário
     n = int(input('Digite um valor: '))
-    # Se o input não estiver na lista, Será adicionado
+    
+    # Se o input não estiver na lista, este será adicionado ao fim dela
     if n not in numeros:
         numeros.append(n)
-        # Imprimindo mensagem
+        
+        # Imprimindo mensagem de aviso
         print('Este valor será adicionado á lista.')
+        
     # caso contrário, não faça nada.(O número não será adicionado)
     else:
+        
         # Imprimindo msg alertando o usuário sobre o input repetido
         print('Valores duplicados não serão adicionados.')
 
-    # Variável solicitante de continuação do programa
+    # Variável solicitante de continuação ou encerramento do programa
     r = str(input('Deseja continuar?\n'
                   '[Sim/Não]: ')).upper().strip()[0]
 
     # Condicional caso o usuário deseje encerrar o programa
     if r in 'Nn':
+        
         # Interrompendo o programa
         break
-# Imprimindo o nosso separador
+        
+# Imprimindo o nosso separador(legibilidade no print)
 print('-=' * 30)
 
 # ordenando a nossa lista antes de imprimir
 numeros.sort()
+
 # Imprimindo a lista
 print(f'Você digitou os valores {numeros}')
