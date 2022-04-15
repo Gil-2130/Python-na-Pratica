8bcb1d2102c70aaca63b07dd4c4384e4bf0c459b
Giliard P. Santos <gil2614@live.com>
2022-04-15 13:28:41 -0300

    Dissecando uma Variável

---
+++ Aula03/Dissecando variavel.py
@@ -0,0 +1,12 @@
+"""
+Identificando o tipo de dado que recebemos no input e todos os métodos disponiveis
+"""
+
+a = input('Digite algo: ')
+print('O tipo primitivo deste dado é', type(a))
+print('Possui espaços? ', a.isspace())
+print('Pode ser convertido para numero? ', a.isdigit())
+print('É um dado alafabetico? ', a.isalpha())
+print('É alfanumérico? ', a.isalnum())
+print('É um dado capitalizado? ', a.istitle())
+print('É um numero? ', a.isnumeric())
