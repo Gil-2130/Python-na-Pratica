8b3f04456f88af60a2eae6420eedd479ef79ad32
Giliard P. Santos <gil2614@live.com>
2022-04-20 20:23:22 -0300

    Desafio002

--- Exercicios/Ex002.py
+++ Exercicios/Ex002.py
@@ -4,8 +4,10 @@ Exercício 2 ->Crie um Programa que eia o nome de uma pesooa de mostre uma mensa
 """
 
 # Respostas com várias saídas formatadas
-
+# Obtendo o nome e armazenando em uma variável
 nome = input('Qual é o seu nome? ')
+
+# Imprimindo de várias formas diferentes
 print(f'Olá {nome}, Seja Bem vindo(a) ao Python!')
 print('============================')
 print('Olá {}, Seja bem vindo!'.format(nome))
