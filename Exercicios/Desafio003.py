9026c9b5f12486ccb9a1674373c8a0ca22f07caa
Giliard P. Santos <gil2614@live.com>
2022-04-20 20:25:07 -0300

    Desafio003

--- Exercicios/Desafio003.py
+++ Exercicios/Desafio003.py
@@ -2,9 +2,13 @@
 Desafio
 -> Crie um programa que leia dois numeros e mostre a soma entre eles.
 """
+# Variável para armazenar um número
 n1 = int(input('Digite um número: '))
+# Variável para armazenar o segundo valor
 n2 = int(input('Digite outro numero: '))
+# Realizando a soma das duas variáveis
 s = n1 + n2
+# Imprimindo os resultados
 print('A soma entre {} e {}, é {}.'.format(n1, n2, s))
 print('===================================')
 
