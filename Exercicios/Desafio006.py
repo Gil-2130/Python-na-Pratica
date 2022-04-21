e54a690af1518e6707aaa00cb9131057b028a9ba
Giliard P. Santos <gil2614@live.com>
2022-04-21 19:49:36 -0300

    Desafio006

--- Exercicios/Desafio006.py
+++ Exercicios/Desafio006.py
@@ -1,10 +1,17 @@
 """
 Crie um algoritmo que leia um número e mostre o seu dobro, triplo e Raís quadrada
 """
+# Obtendo e armazenando o input do usuário
 n = int(input('Digite um número: '))
+
+# Variável para armazenar o dobro
 dobro = n * 2
+# Variável para armazenar o triplo
 triplo = n * 3
+# variável para armazenar a raíz quadrada
 raiz = n ** 2
+
+# Imprimindo os Resultados
 print('O dobro de {} é {}'.format(n, dobro))
 print('=' * 20)
 print('O Triplo de {} é {}'.format(n, triplo))
