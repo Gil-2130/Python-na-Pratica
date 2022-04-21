52dae2412637eac716bb7bbb9d7387f759f2280b
Giliard P. Santos <gil2614@live.com>
2022-04-21 20:04:57 -0300

    Desafio010

--- Exercicios/Desafio010.py
+++ Exercicios/Desafio010.py
@@ -5,6 +5,9 @@ e mostre quantos dolares ela pode comprar
 
 # 1 Dólar (EUA) = 5,26 Real Brasileiro
 
+# Input para receber os valores em reais
 reais = float(input('Quantos reais você tem na carteira? R$'))
+# Realizando a conversão
 dolar = reais / 5.26
+# Imprimindo os resultados
 print('Você com {}R$ Reais poderia comprar {:.2f}R$'.format(reais, dolar))
