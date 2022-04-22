ad82cd0ee28673979f9116508acf91fcff0560d4
Giliard P. Santos <gil2614@live.com>
2022-04-22 20:31:33 -0300

    Desafio013

--- Exercicios/Desafio013.py
+++ Exercicios/Desafio013.py
@@ -8,5 +8,6 @@ sal = float(input('Qual o seu salario? '))
 #n_sal = sal + (sal * 0.15)
 # Calculando o novo salário
 n_sal = sal + (sal * 15 / 100)
+# Imprimindo os resultados obtidos
 print('Seu antigo salário era de {}$\n'.format(sal),
       'Agora seu novo salário com ajuste de 15% é de {}$'.format(n_sal))
