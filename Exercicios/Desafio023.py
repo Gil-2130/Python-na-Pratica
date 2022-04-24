9d3d9e4525dece1fed093115b830ab30fc594128
Giliard P. Santos <gil2614@live.com>
2022-04-24 19:27:32 -0300

    Desafio023

--- Exercicios/Desafio023.py
+++ Exercicios/Desafio023.py
@@ -9,11 +9,18 @@ CENTENA: 8
 MILHAR: 1
 """
 
+# Solicitado o input ao usuário
 n1 = int(input('Digite um numéro entre 0 e 9999: '))
+# Calculando a unidade
 u = n1 // 1 % 10
+# Calculando as dezenas
 d = n1 // 10 % 10
+# Calculando as centenas
 c = n1 // 100 % 10
+# calculando os milhares
 m = n1 // 1000 % 10
+
+# Imprimindo resultados
 print('UNIDADE: {}'.format(u))
 print('DEZENA: {}'.format(d))
 print('CENTENA: {}'.format(c))
