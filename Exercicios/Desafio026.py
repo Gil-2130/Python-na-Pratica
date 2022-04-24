2bc3770d76c57760f0d2505416b6c1348a8c8d73
Giliard P. Santos <gil2614@live.com>
2022-04-24 19:30:24 -0300

    Desafio026

--- Exercicios/Desafio026.py
+++ Exercicios/Desafio026.py
@@ -1,8 +1,10 @@
 """
+
 Crie um programa que leia uma frase pelo teclado e mostre:
 1. Quantas vezes aparece a letra A
 2. Em que posição ela aparece a primeira vez.
 3. Em que posição ela aparece a ultima vez
+
 """
 
 # Obtendo a frase
