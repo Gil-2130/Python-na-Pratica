f3be6ecbd9e121d3ecbdf38fdbc4ad9a4aff38c8
Giliard P. Santos <gil2614@live.com>
2022-04-24 19:29:13 -0300

    Desafio025

--- Exercicios/Desafio025.py
+++ Exercicios/Desafio025.py
@@ -2,5 +2,8 @@
 Crie um programa que leia o nome de uma pessoa e diga
 se ela tem SILVA no nome.
 """
+
+# Obtendo o nome da pessoa
 nome = str(input('Qual o seu nome? ')).strip()
+# Imprimindo resultados
 print('Seu nome tem Silva? {}'.format('silva' in nome.lower()))
