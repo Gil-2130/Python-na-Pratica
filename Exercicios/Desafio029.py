5d0451084a4ef68b93b839236e87d659dd43cf80
Giliard P. Santos <gil2614@live.com>
2022-04-25 20:04:55 -0300

    Desafio029

--- Exercicios/Desafio029.py
+++ Exercicios/Desafio029.py
@@ -6,10 +6,17 @@ Se ele ultrapassar 80km/h, mostre uma msg dizendo que ele foi multado.
 A multa irá custar R$7,00 por cada km acima do limite.
 
 """
+
+# Obtendo velocidade
 vel = float(input('Qual é a velocidade do veículo? '))
+# Definindo limite de velocidade
 lim = 80
+# Condicional para aplicação de multa e seu respectivo valor
 if vel > 80:
+    # Calculando o valor da multa
     mult = (vel - lim)*7
+    # Imprimindo resultados
     print('Você foi MULTADO! O valor da multa será R${:.0f}'.format((mult)))
+    
 else:
     print('Muito bem! Respeite o limite de velocidade.')
