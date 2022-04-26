b7aa5b1473e8cdd43ef78575d2f6f47eec1acdbf
Giliard P. Santos <gil2614@live.com>
2022-04-26 20:02:14 -0300

    Desafio033

--- Exercicios/Desafio033.py
+++ Exercicios/Desafio033.py
@@ -2,12 +2,14 @@
 Faça um programa que leia 03 numeros e mostre qual é o maior e qual é o menor.
 """
 
+# Obtendo os inputs
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro numero: '))
 n3 = int(input('Digite o ultimo numero: '))
 
 # Verificando o menor valor
 menor = n1
+# condicional para obter o menor valor
 if n2 < n1 and n2 < n3:
     menor = n2
 if n3 < n1 and n3 < n2:
