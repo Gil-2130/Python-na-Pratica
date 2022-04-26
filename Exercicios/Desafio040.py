2995a940f59e9e1f4e45df0809febc1d57ef5eaa
Giliard P. Santos <gil2614@live.com>
2022-04-26 20:17:29 -0300

    Desafio040

--- Exercicios/Desafio040.py
+++ Exercicios/Desafio040.py
@@ -6,9 +6,14 @@ mostrando uma mensagem no final, de acordo coma média atingida:
  - Média entre 5 e 6.9: RECUPERAÇÃO
  - Média 7 ou superior: APROVADO
 """
+
+# Obtendo as notas
 n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digie a segunda nota: '))
+
+# Calculando a média
 media = (n1 + n2) / 2
+# Condicional para aprovação
 if media < 5:
     print('REPROVADO! Estude mais')
 elif media >= 5 and media <= 6.9:
