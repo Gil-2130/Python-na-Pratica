128837e8ce78ce518daabfc399be658a7f02d4e8
Giliard P. Santos <gil2614@live.com>
2022-04-27 20:25:39 -0300

    Desafio046

--- Exercicios/Desafio046.py
+++ Exercicios/Desafio046.py
@@ -3,7 +3,7 @@ Faça um programa que mostre na tela uma contagem regressiva para o estouro
 de fogos de artificio, indo de 10 até 0, com um pausa de 1 segundo entre eles.
 """
 
-# Importando módulo para pausar a contagem regessiva
+# Importando módulo para deixar lenta a contagem regressiva
 from time import sleep
 
 # laço de contagem regressiva
