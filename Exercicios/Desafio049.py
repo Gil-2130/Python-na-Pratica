f01f01beadb2b2515b08502b7bf538194051ced9
Giliard P. Santos <gil2614@live.com>
2022-04-28 19:52:19 -0300

    Desafio049

--- Exercicios/Desafio049.py
+++ Exercicios/Desafio049.py
@@ -3,10 +3,15 @@ Refaça o desafio 009, mostrando a tabuada de um número que o usuário escolher
 porém utilizando o laço for.
 """
 
+# Obtendo e arazenando o input do usuário
 num = int(input('Digite um número e sua tabuada(x) será calculada: '))
+# Laço dentro de um range
 for i in range(0, 11):
+    # d recebe cada elemento do laço dentro de um range
     d = i
+    # Cada elemento do laço é multiplicado pelo valor digitado anteriormente
     i = i * num
+    # Imprimindo resultados
     print('{} x {} = {}'.format(num, d, i))
 
 
