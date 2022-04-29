33afe8bed716f3fa8067655c1921a8ed789012c1
Giliard P. Santos <gil2614@live.com>
2022-04-29 19:45:26 -0300

    Desafio054

--- Exercicios/Desafio054.py
+++ Exercicios/Desafio054.py
@@ -1,6 +1,8 @@
 """
+
 Crie um programa que leia o ano de nascimento de 07 pessoas. No final, mostre quantas
 pessoas ainda não atingiram a maioridade e quantas já são maiores.
+
 """
 
 # Importando módulo para trabalhar com datas
