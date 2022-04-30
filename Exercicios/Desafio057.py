cf9305dadc3ba3542ae30d66d4957e74c17136a3
Giliard P. Santos <gil2614@live.com>
2022-04-30 12:17:05 -0300

    Desafio057

--- Exercicios/Desafio057.py
+++ Exercicios/Desafio057.py
@@ -4,6 +4,7 @@ Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores M ou 
 Caso esteja errado, peça a digitação novamente até ter um valor correto.
 """
 
+# Variável com os sexos
 sexo = 'M', 'F'
 
 
