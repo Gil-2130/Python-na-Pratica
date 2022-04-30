a4947e237cdb6ce1f3baa8ce2e6e14a9635f6316
Giliard P. Santos <gil2614@live.com>
2022-04-30 12:20:46 -0300

    Desafio058

--- Exercicios/Desafio058.py
+++ Exercicios/Desafio058.py
@@ -1,7 +1,9 @@
 """
+
 Melhore o jogo do DESAFIO28 onde o computador vai pensar em um numero entre 0 e 10
 
 Porém, o player tentara acertar, mostrando no final, quantos palpites foram necessários para acertar
+
 """
 
 # Importando módulo para criar numeros aleatórios
