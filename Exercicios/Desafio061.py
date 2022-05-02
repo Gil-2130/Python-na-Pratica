6801d1848fdc40da2ae92c10c8140319416561ac
Giliard P. Santos <gil2614@live.com>
2022-05-02 19:15:10 -0300

    Desafio061

--- Exercicios/Desafio061.py
+++ Exercicios/Desafio061.py
@@ -1,11 +1,13 @@
 """
-Refaça o desafio 051, lendo o primeiro termo e a razão de uma PA, mostrando os 10
+Refaça o desafio051, lendo o primeiro termo e a razão de uma PA, mostrando os 10
 primeiros termos da progressão usando a estrutura WHILE
 """
 
+# Imprimindo msg da solução
 print('Gerando PA')
 print('=-=' * 10)
 
+# Obtendo o primeiro termo
 prim = int(input('Primeiro termo: '))
 razao = int(input('Razão da PA: '))
 termo = prim
