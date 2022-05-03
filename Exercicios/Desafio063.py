7ce95b2360ce268a3941fce20fbbb9aa3b6f9b1b
Giliard P. Santos <gil2614@live.com>
2022-05-03 20:15:30 -0300

    Desafio063

--- Exercicios/Desafio063.py
+++ Exercicios/Desafio063.py
@@ -1,8 +1,10 @@
 """
+
 Escreva um programa que leia um número n inteiro e mostre na tela os n primeiros
 elementos de uma sequência de Fibonacci.
 
 Ex: 0 -> 1 -> 1 -> 2 -> 3 -> 5 -> 8
+
 """
 
 # Separadores e mensagem do programa
