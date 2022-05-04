df9361422186de5280389aa7782af012e826a28b
Giliard P. Santos <gil2614@live.com>
2022-05-04 20:19:56 -0300

    Desafio069

--- Exercicios/Desafio069.py
+++ Exercicios/Desafio069.py
@@ -1,3 +1,4 @@
+"""
 Crie um programa que leia a idade eo sexo de várias pessoas.
 
 A cada pessoa cadastrada, o programa deverá pergntar se o usuário quer
@@ -5,7 +6,9 @@ ou não continuar. No final, mostre:
 
 A) Quantas pessoas tem mais de 18 anos.
 B) Quantos Homens foram cadastrados.
-C) Qauntas mulheres tem menos de 20 anos
+C) Quantas mulheres tem menos de 20 anos
+
+"""
 
 # REsposta do Professor
 
