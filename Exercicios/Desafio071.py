d65f99c4f4b0190ed2476b7e167dedc7b0987d65
Giliard P. Santos <gil2614@live.com>
2022-05-04 20:27:32 -0300

    Desafio071

--- Exercicios/Desafio071.py
+++ Exercicios/Desafio071.py
@@ -1,8 +1,11 @@
-"""Crie um programa que simule o funcionamento de um caixa eletrônico.
+"""
+
+Crie um programa que simule o funcionamento de um caixa eletrônico.
 No inicio, pregunte ao usuário qual será o valor a ser sacado(inteiro)
 Seu programa deve informar quantas cédulas de cada valor serão entregues.
 
 Obs; Considere que o caixa possui cédulas de 50, 20, 10 e 1
+
 """
 # Imprimindo espaçadores (organização do código
 print('='*30)
