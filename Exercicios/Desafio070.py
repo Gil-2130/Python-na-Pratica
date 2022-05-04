13f4db5cba95d477adb60ff543522adddfc26ae4
Giliard P. Santos <gil2614@live.com>
2022-05-04 20:20:41 -0300

    Desafio070

--- Exercicios/Desafio070.py
+++ Exercicios/Desafio070.py
@@ -1,4 +1,5 @@
 """
+
 Crie um programa que leia o nome e o preço de vários produtos.
 O programa deverá perguntar se o usuário vai continuar.
 No final mostre;
@@ -6,6 +7,7 @@ No final mostre;
 A) Qual é o total gasto na compra.
 B) Quantos produtos custam mais de R$1000
 C) Qual é o nome do produto mais vendido?
+
 """
 # variaveis para armazenar valores
 total = 0
