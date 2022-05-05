513a481e427842adfc8f6565a424423a761603b6
Giliard P. Santos <gil2614@live.com>
2022-05-05 20:04:57 -0300

    Desafio072

--- Exercicios/Desafio072.py
+++ Exercicios/Desafio072.py
@@ -1,11 +1,9 @@
 """
-
-crie um programa que tenha um tupla totalmente preenchida com uma
+Crie um programa que tenha um tupla totalmente preenchida com uma
 contagem por extenso de zero até 20.
 
 Seu programa deverá ler um número pelo teclado (entre 0 e 20)
 e mostrá-lo por extenso
-
 """
 
 
