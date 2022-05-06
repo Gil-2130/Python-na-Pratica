14e5db610f26b9fe1a8a5e8878cb87689d77edc0
Giliard P. Santos <gil2614@live.com>
2022-05-06 19:54:52 -0300

    Desafio078

--- Exercicios/Desafio078.py
+++ Exercicios/Desafio078.py
@@ -3,6 +3,7 @@ Faça um programa que leia 05 valores numéricos e guarde em uma lista.
 
 No final mostre; Qual foi o menor e maior valor digitado e as suas respectivas
 posições na lista
+
 """
 # Minha Solução
 
