de06361520d20ed5b47b62a61bd01977d3a03807
Giliard P. Santos <gil2614@live.com>
2022-05-06 19:55:46 -0300

    Desafio080

--- Exercicios/Desafio080.py
+++ Exercicios/Desafio080.py
@@ -1,8 +1,10 @@
 """
+
 Crie um programa onde o usuário possa digitar 05 valores numéricos
 e cadastre-os em uma lista, já na posição correta de inserção(sem usar o sort())
 
 No final, mostre a lista ordenada na tela.
+
 """
 
 # Criando uma lista vazia
