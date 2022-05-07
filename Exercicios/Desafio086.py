bf3be07d15520d9c3da9182158c0ed631bd42724
Giliard P. Santos <gil2614@live.com>
2022-05-07 20:23:25 -0300

    Desafio086

--- Exercicios/Desafio086.py
+++ Exercicios/Desafio086.py
@@ -1,8 +1,10 @@
 """
+
 Crie um programa que crie uma matriz 3 x 3
 e preencha com valores lidos pelo teclado.
 
 No final, mostre a matriz na tela, com a formatação correta
+
 """
 
 # Criando variavel que irá armazenar as listas
