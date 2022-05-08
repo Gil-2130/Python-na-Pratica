70bedc5be17f97a0871ddf34a1e1064b024faf73
Giliard P. Santos <gil2614@live.com>
2022-05-08 09:36:45 -0300

    Desafio088

--- Exercicios/Desafio088.py
+++ Exercicios/Desafio088.py
@@ -1,11 +1,9 @@
 """
-
 Faça um programa que ajude um jogador da megasena a criar palpites.
 
 O programa vai perguntar quantos jogos serão gerados e vai sortear
 6 números entre e 1 e 60 para cada jogo,
 cadastrando tudo em uma lista composta.
-
 """
 from random import randint
 
