a84deb90ef6494b3a2e0e74237e2f413da1a2bad
Giliard P. Santos <gil2614@live.com>
2022-05-08 09:38:29 -0300

    Desafio091

---
+++ Exercicios/Desafio091.py
@@ -0,0 +1,6 @@
+"""
+Crie um programa onde 04 jogadores joguem um dado e tenham resultados aleatórios.
+Guarde esses resultados em um dicionário.
+No final coloque esse dicionário em ordem, sabendo que o vencedor
+tirou o maior número do dado
+"""
