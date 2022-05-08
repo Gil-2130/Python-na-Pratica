52513efdafb188326563fdbdd5431efa94780ed0
Giliard P. Santos <gil2614@live.com>
2022-05-08 09:36:20 -0300

    Desafio087

--- Exercicios/Desafio087.py
+++ Exercicios/Desafio087.py
@@ -1,12 +1,11 @@
 """
-
 Aprimore o desafio anterior(desafio086), mostrando no final;
 
 A) A soma de todos os valores pares digitados
 B) A soma dos valores da terceira coluna.
 C) O maior valor da segunda linha.
-
 """
+
 # Criando Matriz (listas dentro de uma lista)
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
