d1f409034a3f7e9e4c495bc10b0eb89a0bb9eaa0
Giliard P. Santos <gil2614@live.com>
2022-05-08 09:38:58 -0300

    Desafio092

---
+++ Exercicios/Desafio092.py
@@ -0,0 +1,6 @@
+"""
+Crie um programa que leia NOME, Ano de Nascimento e carteira de trabalho,
+cadastre-os(com idade) em um dicionário se por acaso a CTPS for diferente de 0,
+o dicionário receberá também o ano de contratação e o salário.
+Calcule e acrescente, além da idade, com quantos anos a pessoa irá se aposentar.
+"""
