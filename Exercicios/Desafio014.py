2437e1107b4f1ba80c42243835baeaa3e0ad651e
Giliard P. Santos <gil2614@live.com>
2022-05-10 20:40:05 -0300

    Desafio014

--- Exercicios/Desafio014.py
+++ Exercicios/Desafio014.py
@@ -4,14 +4,17 @@ digitada em ℃ e converta para ℉
 """
 # Obtendo a temperatura em Celsius
 temp = float(input('Qual a temperatura hoje em ℃? '))
+
 # Calculando e convertendo para Fahrenheit
 fah = temp * 1.8 + 32
 
 # Método 2
 # fah = temp * 9 / 5 + 32
 print('A temperatura em Fahrenheit é de {}℉'.format(fah))
+
 # Obtendo a temperatura
 temp1 = float(input('Qual a temperatura hoje em ℉? '))
+
 # Convertendo
 cels = (temp1 - 32) / 1.8
 print('A temperatura em Celsius é de {}℃'.format(cels))
