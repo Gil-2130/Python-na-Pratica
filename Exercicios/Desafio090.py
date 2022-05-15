32fe030e1fa114b808c19cd379299507da450dc7
Giliard P. Santos <gil2614@live.com>
2022-05-15 13:29:51 -0300

    Desafio090

--- Exercicios/Desafio090.py
+++ Exercicios/Desafio090.py
@@ -17,3 +17,27 @@ for c in range(0, 1):
         print(f'O aluno(a) {nome} teve média acima de 7 e está Aprovado!')
     else:
         print(f'O aluno(a) {nome} teve média abaixo de 7 e está Reprovado!')
+        
+        
+        #=============== RESPOSTA DO PROFESSOR ==========#
+# Criando um dicionário
+aluno = dict()
+# Adicionando elementos ao dicionário
+aluno['Nome'] = str(input('Digite o seu nome: '))
+aluno['Média'] = float(input(f'Média de {aluno["Nome"]}: '))
+
+# Condicional para aprovação
+if aluno['Média'] >= 7:
+    aluno['Situação'] = 'Aprovado'
+# Condicional para recuperação
+elif aluno['Média'] >= 5  or aluno['Média'] < 7:
+    aluno['Situação'] = 'Recuperação'
+# Caso contrário ele estará reprovado
+else:
+    aluno['Situação'] = 'Reprovado'
+# imprimindo organizador
+print('-=' * 30)
+
+# Laço para obter os itens do nosso dicionário
+for key, value in aluno.items():
+    print(f'   - {key} é igual a {value}')
