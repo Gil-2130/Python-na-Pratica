dbc70278df1ac90db519efa2a13522afd3954d78
Giliard P. Santos <gil2614@live.com>
2022-05-18 17:35:34 -0300

    Add files via upload

---
+++ Exercicios/Desafio099.py
@@ -0,0 +1,5 @@
+"""
+Faça um programa que tenha a função chamada maior(),
+esta receberá vários parâmetros com valores inteiros.
+Nosso programa tem que analisar todos os valores  e dizer qual deles é o maior
+"""
