658c7e0020bd7f0e290c85f15fcc1a432d7835ad
Giliard P. Santos <gil2614@live.com>
2022-05-18 17:40:06 -0300

    Desafio097

--- Exercicios/Desafio097.py
+++ Exercicios/Desafio097.py
@@ -3,3 +3,16 @@ Faça um programa que tenha uma função chamada escreva(),
 a mesma receberá um text qualquer como parâmetro e irá imprimir a mensagem
 com tamanho adaptável.
 """
+
+
+# Criando função
+def escreva(msg):
+    # adaptando o tamanho da msg
+    tam = len(msg) + 4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+# Programa principal
+escreva('Python é incrível!!')
