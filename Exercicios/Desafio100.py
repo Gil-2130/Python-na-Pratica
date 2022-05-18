4c2a8990b893c394ef7bc72be349f9f9b6cb7dd4
Giliard P. Santos <gil2614@live.com>
2022-05-18 17:38:30 -0300

    Add files via upload

---
+++ Exercicios/Desafio100.py
@@ -0,0 +1,7 @@
+"""
+Faça um programa que tenha uma LISTA chamada NUMEROS[] e duas funções
+chamadas SORTEIA() e SOMAPAR().
+A primeira função vai sortear 5 números e colocá-los dentro da lista
+e a segunda função vai mostrar a SOMA entre todos os valores PARES
+sorteados pela função anterior.
+"""
