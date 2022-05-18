1e8f9fe0d89c6b3655ceda242b0cca50ecf309b3
Giliard P. Santos <gil2614@live.com>
2022-05-18 17:34:53 -0300

    Add files via upload

---
+++ Exercicios/Desafio098.py
@@ -0,0 +1,9 @@
+"""
+Faça um programa que tenha a função chamada contador(),
+que receberá 03 parâmetros;INÍCIO, FIM e PASSO e realize a contagem.
+
+Seu programa tem que realizar 03 contagens através da função criada:
+A) De 1 até 10, de 1 em 1
+B) DE 10 até 0 , de 2 em 2
+C) Uma contagem personalizada
+"""
