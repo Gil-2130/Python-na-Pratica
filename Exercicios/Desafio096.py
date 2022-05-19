eb7da78efdd2fc25214ce7a3075c8c1a46c5bb54
Giliard P. Santos <gil2614@live.com>
2022-05-19 08:35:23 -0300

    Desafio096

--- Exercicios/Desafio096.py
+++ Exercicios/Desafio096.py
@@ -1,7 +1,9 @@
 """
+
 Faça um programa que tenha uma função chamada área().
 A mesma deverá receber as dimensões de um terreno retangular(largura e comprimento)
 Depois imprima a área do tereno
+
 """
 
 
