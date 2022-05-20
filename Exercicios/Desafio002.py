1ce87751a591fa35d4a1680f87d53fc5f58f0b4a
Giliard P. Santos <gil2614@live.com>
2022-05-20 10:09:22 -0300

    Desafio002

--- Exercicios/Desafio002.py
+++ Exercicios/Desafio002.py
@@ -4,6 +4,7 @@ Exercício 2 ->Crie um Programa que eia o nome de uma pesooa de mostre uma mensa
 """
 
 # Respostas com várias saídas formatadas
+
 # Obtendo o nome e armazenando em uma variável
 nome = input('Qual é o seu nome? ')
 
