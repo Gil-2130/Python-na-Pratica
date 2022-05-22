e214ad36077affd2d3ee50fa4f89b5b980c953ab
Giliard P. Santos <gil2614@live.com>
2022-05-21 21:41:02 -0300

    Desafio099

--- Exercicios/Desafio099.py
+++ Exercicios/Desafio099.py
@@ -1,10 +1,9 @@
 """
-
 Faça um programa que tenha a função chamada maior(),
 esta receberá vários parâmetros com valores inteiros.
 Nosso programa tem que analisar todos os valores e dizer qual deles é o maior.
-
 """
+
 # Importando biblioteca para dar sleep
 from time import sleep
 
