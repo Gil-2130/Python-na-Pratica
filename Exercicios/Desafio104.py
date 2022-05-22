44cfdff99b58d2588d814f6a7fbe16d1ebd7e912
Giliard P. Santos <gil2614@live.com>
2022-05-22 16:28:28 -0300

    Add files via upload

---
+++ Exercicios/Desafio104.py
@@ -0,0 +1,6 @@
+'''
+Crie um programa que tenha a função leiaINT(), que vai funcionar de forma semelhante à
+função input() do Python, só ue fazendo a validação aceitar apenas um valor numérico.
+EX:
+n = leiaINT('Digite um numero: ')
+'''
