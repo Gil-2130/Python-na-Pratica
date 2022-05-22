aee93c2828507818eac00ca3b43b86cd41efd4d1
Giliard P. Santos <gil2614@live.com>
2022-05-22 16:26:31 -0300

    Desafio100

--- Exercicios/Desafio100.py
+++ Exercicios/Desafio100.py
@@ -5,6 +5,7 @@ A primeira função vai sortear 5 números e colocá-los dentro da lista
 e a segunda função vai mostrar a SOMA entre todos os valores PARES
 sorteados pela função anterior.
 """
+
 # Importando biblioteca para trabalhar com números randomizados
 from random import randint
 # Desacelerando a execução do print
