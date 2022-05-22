983efc84898a4059c7d44a170ba7bdce1382573f
Giliard P. Santos <gil2614@live.com>
2022-05-22 16:27:16 -0300

    Add files via upload

---
+++ Exercicios/Desafio101.py
@@ -0,0 +1,6 @@
+'''
+Crie um programa que tenha uma função chamada voto()
+Esta irá receber como parâmetro o ano de nascimento de uma pessoa.
+Retornando um valor literal indicando se uma pessoa
+tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições.
+'''
