39b991af498f63dfc938ce24be0eada972c74f91
Giliard P. Santos <gil2614@live.com>
2022-05-23 22:42:45 -0300

    Update Desafio103.py

--- Exercicios/Desafio103.py
+++ Exercicios/Desafio103.py
@@ -1,8 +1,10 @@
 '''
+
 Faça um programa que contenha uma função chamada ficha(),
 que receba dois parãmtros opcionais:
 O nome de um jogador e quantos gols ele marcou.
 
 O programa deverá ser capaz de mostrar a ficha o jogador,
 mesmo que algum dado não tenha sido informado corretamente.
+
 '''
