648cd90f491e328795281b18546d3425d2ed091f
Giliard P. Santos <gil2614@live.com>
2022-05-24 08:58:40 -0300

    Desafio101

--- Exercicios/Desafio101.py
+++ Exercicios/Desafio101.py
@@ -6,3 +6,30 @@ Retornando um valor literal indicando se uma pessoa
 tem voto NEGADO, OPCIONAL ou OBRIGATÓRIO nas eleições.
 
 '''
+
+
+# Criando função
+def voto(ano):
+    # importando biblioteca para trabalhar com datas
+    from datetime import date
+
+    # obtendo ano atual
+    atual = date.today().year
+
+    # Calculando idade
+    idade = atual - ano
+
+    # Condicionas de obrigatoriedade de votação
+    if idade < 16:
+        return f'Com {idade} anos: NÃO VOTA!'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL!'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATÓRIO!'
+
+
+# Obtendo a idade do usuário
+nasc = int(input('Em que ano você nasceu?: '))
+
+# Imprimido programa
+print(voto(nasc))
