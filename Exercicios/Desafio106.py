e758730aea4c76f48b6d6ed2d9705954a6889277
Giliard P. Santos <gil2614@live.com>
2022-05-23 22:43:55 -0300

    Update Desafio106.py

--- Exercicios/Desafio106.py
+++ Exercicios/Desafio106.py
@@ -1,7 +1,9 @@
 '''
+
 Faça um programa que utilize o Interactive Help do Python.
 O usuário vai digitar o comando e o manual vai aparecer.
 Quando o usuário digitar a palavra FIM, o programa se encerrará.
 
 OBS: use cores.
+
 '''
