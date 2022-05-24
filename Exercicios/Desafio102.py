433b25ef7bc96a9c615496914cb45c2c0cc882ad
Giliard P. Santos <gil2614@live.com>
2022-05-23 22:42:03 -0300

    Update Desafio102.py

--- Exercicios/Desafio102.py
+++ Exercicios/Desafio102.py
@@ -1,6 +1,8 @@
 '''
+
 Crie um programa que tenha uma função fatorial() que receba dois parâmetros:
 A) O primeiro que indique o número a calcular
 B) O segundomserá um valor lógico chamado SHOW, indicando se será mostrado
 ou não na tela o processo de calculo do fatorial.
+
 '''
