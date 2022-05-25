83b9b3bf15b256df3d17ff01f70f421daee76b20
Giliard P. Santos <gil2614@live.com>
2022-05-25 10:44:09 -0300

    Desafio104

--- Exercicios/Desafio104.py
+++ Exercicios/Desafio104.py
@@ -6,3 +6,34 @@ EX:
 n = leiaINT('Digite um numero: ')
 
 '''
+
+
+# Criando função
+def leiaINT(msg):
+    # Inicializando variável booleana de valor FALSO
+    ok = False
+    # Inicializando variável de valor zero
+    valor = 0
+    # laço infinito que receberá valores inteiros
+    while True:
+        # Obtendo os valores
+        numero = str(input(msg))
+        # Condicional se o valor digitado é um inteiro
+        if numero.isnumeric():
+            # Variável VALOR agora receberá o inteiro digitado
+            valor = int(numero)
+            # Variável booleana OK receberá valor TRUE
+            ok = True
+        # caso contrário...
+        else:
+            # imprimindo msg solicitando um valor inteiro ao usuário
+            print(f'\033[0;31mERRO! Valor digitado inválido! Digite apenas números.\033[m')
+        # Condicional para interromper o loop infinito
+        if ok:
+            break
+    return valor
+
+
+# Programa principal
+numero = leiaINT('Digite um número: ')
+print(f'Você digitou {numero}.')
