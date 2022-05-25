79c6404a5fba8d2a3bdf668fdf281ab3ee4ec47c
Giliard P. Santos <gil2614@live.com>
2022-05-25 10:06:55 -0300

    Desafio103

--- Exercicios/Desafio103.py
+++ Exercicios/Desafio103.py
@@ -8,3 +8,26 @@ O programa deverá ser capaz de mostrar a ficha o jogador,
 mesmo que algum dado não tenha sido informado corretamente.
 
 '''
+
+
+# Criando função
+def ficha(jogador='<Desconhecido>', gol=0):
+    print(f'O jogador {jogador.upper()} fez {gol} gols na partida.')
+
+
+# Programa principal
+nome = str(input('Nome do Jogador: '))
+gols = str(input('Número de Gols: '))
+
+# Condicional para verificar se gols é um valor numérico
+if gols.isnumeric():
+    # Se sim, converteremos para inteiro
+    gols = int(gols)
+# Caso contrário, o valor 0 será atribuído a essa variável.
+else:
+    gols = 0
+# Se o nome do jogador receber um valor vazio
+if nome.strip() == '':
+    ficha(gol=gols)
+else:
+    ficha(nome, gols)
