4c187a9f57a67b51009b18f8a3dd84756d35fec5
Giliard P. Santos <gil2614@live.com>
2022-05-26 09:37:58 -0300

    Desafio105

--- Exercicios/Desafio105.py
+++ Exercicios/Desafio105.py
@@ -11,3 +11,39 @@ de alunos e vai retornar um dicionário com as seguintes informações:
 Adicione também as DOCSTRINGS da função.
 
 '''
+
+
+# Criando função que receberá vária notas como parâmetro e situação(opcional)
+def notas(*n, situacao=False):
+    """
+    => Função para analisar as notas e situações de alunos.
+    :param n: Receberá várias notas.
+    :param situacao: (opcional) mostra a situação do aluno.
+    :return: retorna um dicionário contendo a nota mais baixa, média das notas, nota máxima e total de notas.
+    """
+    # Criando dicionário
+    r = dict()
+    # Total de notas
+    r['Total'] = len(n)
+    # Maior nota
+    r['Maior'] = max(n)
+    # Menor nota
+    r['Menor'] = min(n)
+    # Média das notas
+    r['Média'] = sum(n) / len(n)
+
+    # Parâmetro opcional para imprimir situação
+    if situacao:
+        if r['Média'] > 7:
+            r['Situação'] = 'Aprovado'
+        elif 5 < r['Média'] < 7:
+            r['Situação'] = 'Recuperação'
+        else:
+            r['Situação'] = 'Reprovado'
+
+    return r
+
+
+# Programa Principal
+resp = notas(5.5, 5.2, 9, 8.5)
+print(resp)
