02b6a24c2241ede82fd74526103b82e260392db9
Giliard P. Santos <gil2614@live.com>
2022-05-30 11:15:22 -0300

    Add files via upload

---
+++ Exercicios/Desafio107.py
@@ -0,0 +1,9 @@
+"""
+Crie um módulo chamada moeda.py que tenha as funções incorporadas;
+1- Aumentar()
+2- Diminuir()
+3- Dobro()
+4- Metade
+
+Faça também um programa que importe esse módulo e use algumas dessas funções
+"""
