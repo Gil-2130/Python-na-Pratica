3c18fd3e30e44912bc860f009bee8811ed6f81ff
Giliard P. Santos <gil2614@live.com>
2022-05-30 11:16:24 -0300

    Add files via upload

---
+++ Exercicios/Desafio109.py
@@ -0,0 +1,5 @@
+"""
+Modifique funções qe foram criadas no desafio107 para elas aceitarem um parâmetro a mais,
+informando se o valor retornado por elas vai ser ou não formatado pela função moeda(),
+desenvolvida no desafio108.
+"""
