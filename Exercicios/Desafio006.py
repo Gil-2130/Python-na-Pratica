a850bff4c1cdfee5d65ceec40dccc6c8e169ccce
Giliard P. Santos <gil2614@live.com>
2022-07-11 22:49:04 -0300

    dobro, triplo e raiz quadrada de um número.

--- Exercicios/Desafio006.py
+++ Exercicios/Desafio006.py
@@ -7,8 +7,10 @@ n = int(input('Digite um número: '))
 
 # Variável para armazenar o dobro
 dobro = n * 2
+
 # Variável para armazenar o triplo
 triplo = n * 3
+
 # variável para armazenar a raíz quadrada
 raiz = n ** 2
 
