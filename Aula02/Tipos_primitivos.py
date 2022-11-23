8b2fe97dace28cf845e9c6a4b15a6babfd6da1a5
Giliard P. Santos <gil2614@live.com>
2022-11-23 13:02:02 -0300

    Update Tipos_primitivos.py

--- Aula02/Tipos_primitivos.py
+++ Aula02/Tipos_primitivos.py
@@ -1,6 +1,6 @@
 """
   Nessa aula iremos trabalhar com tipos primitivos na prática;
-  Em Python existem 04 tipos primitivos; int, float, bool e str.
+    Em Python existem 04 tipos primitivos; int, float, bool e str.
     int = numeros inteiros
     float = numeros flutuantes(com casas decimais)
     bool = True ou False
