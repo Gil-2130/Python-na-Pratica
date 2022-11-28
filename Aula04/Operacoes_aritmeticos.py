7f31f39f48cb5eb31a46ce5cc92ed72739f30a6d
Giliard P. Santos <gil2614@live.com>
2022-11-28 13:15:51 -0300

    Update Operacoes_aritmeticos.py

--- Aula04/Operacoes_aritmeticos.py
+++ Aula04/Operacoes_aritmeticos.py
@@ -6,7 +6,7 @@
     (*) --> Multiplicação
     (/) --> Divisão
     (**) --> Exponenciação/potenciação ou pow(,)
-  (//) --> Divisão inteira
+    (//) --> Divisão inteira
 (%) --> Resto da divisão/Módulo
 pow(n, (1/2)) --> Calculando a raiz quadrada
 
