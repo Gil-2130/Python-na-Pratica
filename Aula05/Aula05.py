4b55e575e034101379bbb471685e511abb160c08
Giliard P. Santos <gil2614@live.com>
2022-12-04 20:07:33 -0300

    Update Aula05.py

--- Aula05/Aula05.py
+++ Aula05/Aula05.py
@@ -1,6 +1,6 @@
 """
   Importando modulos para calculos numericos (math)
-  sqrt => Raíz quadrada
+    sqrt => Raíz quadrada
   ceil => Arredondar pra cima 5.8 -> 6.0
   floor => Arredondar pra baixo 5.8 -> 5.0
   trunc => truncate, corta na virgula/ponto sem arredondar 5.8 -> 5
