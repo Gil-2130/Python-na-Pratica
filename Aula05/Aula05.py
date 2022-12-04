0be5d26210c165db404227bc463c940a2cb6286a
Giliard P. Santos <gil2614@live.com>
2022-12-04 20:07:48 -0300

    Update Aula05.py

--- Aula05/Aula05.py
+++ Aula05/Aula05.py
@@ -2,7 +2,7 @@
   Importando modulos para calculos numericos (math)
     sqrt => Raíz quadrada
     ceil => Arredondar pra cima 5.8 -> 6.0
-  floor => Arredondar pra baixo 5.8 -> 5.0
+    floor => Arredondar pra baixo 5.8 -> 5.0
   trunc => truncate, corta na virgula/ponto sem arredondar 5.8 -> 5
   pow => potência ou **
   sqrt => Raiz quadrada
