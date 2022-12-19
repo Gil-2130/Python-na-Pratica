222eeb18b810d0060aa218a995a5d49a0c32811e
Giliard P. Santos <gil2614@live.com>
2022-12-19 12:47:29 -0300

    Update Aula12.py

--- Aula12/Aula12.py
+++ Aula12/Aula12.py
@@ -1,4 +1,5 @@
 """
+
 tupla = () -> representada com argumentos entre parenteses(não é necessário, mas é uma boa prática)
 lista = [] -> REpresentada por argumentos entre colchetes
 dicionário = {} -> Representada por elementos dentro de chaves
